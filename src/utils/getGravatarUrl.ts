38b01c08d3b2d95e49206e6bc12ed52c99bcd9df
Marcin <zolkiewski.marcin@gmail.com>
2021-05-06 12:04:51 +0200

    updated demo gravatars

--- src/utils/getGravatarUrl.ts
+++ src/utils/getGravatarUrl.ts
@@ -2,8 +2,17 @@ import { getGravatarUrl as reactGetGravatar } from "react-awesome-gravatar";
 
 export function getGravatarUrl(email: string, size = 50): string {
   let targetEmail = email || "";
-  if (targetEmail === "mrinal.manohar@casperlabs.io") {
+  if (
+    targetEmail === "mrinal.manohar@casperlabs.io" ||
+    targetEmail === "mrinal@casperlabs.io"
+  ) {
     targetEmail = "senecio666@gmail.com";
+  }
+  if (
+    targetEmail === "cliff.sarkin@casperlabs.io" ||
+    targetEmail === "cliff@casperlabs.io"
+  ) {
+    targetEmail = "jeb.titus.lance@gmail.com";
   }
   return reactGetGravatar(email, {
     default: "mp",
