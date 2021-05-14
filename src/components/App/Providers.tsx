6b118e0c43fd1d94e915ab8b016160977070f3bc
Marcin <zolkiewski.marcin@gmail.com>
2021-05-14 22:43:30 +0200

    cleanup

--- src/components/App/Providers.tsx
+++ src/components/App/Providers.tsx
@@ -1,30 +1,16 @@
 import React from "react";
-import { UseWalletProvider } from "use-wallet";
 import { QueryParamProvider } from "use-query-params";
 
-import { EthersProvider, PostSignProvider } from "../../contexts";
-import { CHAIN_ID, RPC_URL } from "../../env";
+import { PostSignProvider } from "../../contexts";
 
 interface IProps {
   children: React.ReactNode;
 }
 
 const Providers = ({ children }: IProps) => (
-  <UseWalletProvider
-    chainId={CHAIN_ID}
-    connectors={{
-      injected: {},
-      walletconnect: {
-        rpcUrl: RPC_URL,
-      },
-    }}
-  >
-    <EthersProvider>
-      <QueryParamProvider>
-        <PostSignProvider>{children}</PostSignProvider>
-      </QueryParamProvider>
-    </EthersProvider>
-  </UseWalletProvider>
+  <QueryParamProvider>
+    <PostSignProvider>{children}</PostSignProvider>
+  </QueryParamProvider>
 );
 
 export default Providers;
